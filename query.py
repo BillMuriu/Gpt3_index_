a08622def0b9d5b20caa5d06ca19e37b73a31f20
unknown <njorogekamau63@gmail.com>
2023-03-20 13:07:35 +0300

    imports

---
+++ query.py
@@ -0,0 +1,7 @@
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import sys
+import os
+
+os.envirom["OPENAI_API_KEY"] = ""
+
