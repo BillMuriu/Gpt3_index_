fec7d203056c81fef4c135916ccfc92bdbc999e8
unknown <njorogekamau63@gmail.com>
2023-03-20 13:49:43 +0300

    it finally worked

--- query.py
+++ query.py
@@ -3,9 +3,9 @@ from langchain import OpenAI
 import sys
 import os
 
-os.envirom["OPENAI_API_KEY"] = ""
+os.environ["OPENAI_API_KEY"] = "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"
 
-def CreatevectorIndex(path):
+def CreateVectorIndex(path):
     max_input = 4096
     tokens = 256
     chunk_size = 600
@@ -14,7 +14,23 @@ def CreatevectorIndex(path):
     prompt_helper = PromptHelper(max_input, tokens, max_chunk_overlap, chunk_size_limit=chunk_size)
 
     # define LLM
-    LLMPredictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-ada-001", max_tokens=tokens))
+    llmPredictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-ada-001", max_tokens=tokens))
 
     #load data
     docs = SimpleDirectoryReader(path).load_data()
+
+    #create index
+    vectorIndex = GPTSimpleVectorIndex(documents=docs, llm_predictor=llmPredictor, prompt_helper=prompt_helper)
+    vectorIndex.save_to_disk('vectorIndex.json')
+    return vectorIndex
+
+vectorIndex = CreateVectorIndex('text_txt')
+
+def answerMe(vectorIndex):
+    vIndex = GPTSimpleVectorIndex.load_from_disk(vectorIndex)
+    while True:
+        prompt = input("Enter your question: ")
+        response = vIndex.query(prompt, response_mode="compact")
+        print(f"Response: {response} \n")
+
+answerMe('vectorIndex.json')
